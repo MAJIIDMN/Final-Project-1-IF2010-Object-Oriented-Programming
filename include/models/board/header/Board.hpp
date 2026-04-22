c31187e391ff903694beef6fdd372b0ae7523883
AthillaZaidan <athillazaidanstudy@gmail.com>
2026-04-23 00:03:50 +0700

    feat: board model

--- include/models/board/header/Board.hpp
+++ include/models/board/header/Board.hpp
@@ -1,12 +1,39 @@
-#ifndef BOARD_HPP
-#define BOARD_HPP
+#ifndef MODELS_BOARD_HEADER_BOARD_HPP
+#define MODELS_BOARD_HEADER_BOARD_HPP
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "utils/Enums.hpp"
+
+class Tile;
+class StreetTile;
+class PropertyTile;
+class ColorGroup;
 
 class Board {
 public:
+	static constexpr int BOARD_SIZE = 40;
+
+	Board();
+	~Board();
+
+	void addTile(std::unique_ptr<Tile> tile);
+	Tile* getTile(int index) const;
+	Tile* getTileByCode(const std::string& code) const;
+	int getSize() const;
+
+	StreetTile* getStreetByCode(const std::string& code) const;
+	PropertyTile* getPropertyByCode(const std::string& code) const;
+
+	void registerColorGroup(ColorGroup* group);
+	ColorGroup* getColorGroup(Color color) const;
+	void updateMonopolies();
 
 private:
-
-protected:
+	std::vector<std::unique_ptr<Tile>> tiles;
+	std::vector<ColorGroup*> colorGroups;
 };
 
 #endif
