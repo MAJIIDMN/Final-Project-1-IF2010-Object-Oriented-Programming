db08e39c284c12a1c1742fa8a00b76f8f92b71b9
AthillaZaidan <athillazaidanstudy@gmail.com>
2026-04-23 00:03:04 +0700

    feat: property tile class

--- include/tile/header/PropertyTile.hpp
+++ include/tile/header/PropertyTile.hpp
@@ -1,14 +1,38 @@
-#ifndef PROPERTY_TILE_HPP
-#define PROPERTY_TILE_HPP
+#ifndef TILE_HEADER_PROPERTY_TILE_HPP
+#define TILE_HEADER_PROPERTY_TILE_HPP
 
-#include "Tile.hpp"
+#include "tile/header/Tile.hpp"
+#include "models/Money.hpp"
+#include "utils/Enums.hpp"
+
+class Player;
 
 class PropertyTile : public Tile {
 public:
+	PropertyTile(int id, const std::string& code, const std::string& name, TileType type,
+				 Money price, Money mortgageValue);
+
+	Money getPrice() const;
+	Money getMortgageValue() const;
+
+	Player* getOwner() const;
+	void setOwner(Player* player);
+
+	PropertyStatus getStatus() const;
+	void setStatus(PropertyStatus status);
+
+	bool isOwned() const;
+	bool isMortgaged() const;
+
+	virtual Money getRent(int diceRoll = 0) const = 0;
+	virtual bool canBeDeveloped() const;
+	virtual int getBuildingLevel() const;
 
 private:
-
-protected:
+	Money price;
+	Money mortgageValue;
+	Player* owner;
+	PropertyStatus status;
 };
 
 #endif
