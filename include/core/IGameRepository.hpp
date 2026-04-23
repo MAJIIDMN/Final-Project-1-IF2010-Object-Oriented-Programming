116aab6a57d0adcb3e88366ca5765df13c10a221
Jason Edward Salim <jasonedwardsalim@gmail.com>
2026-04-23 19:24:38 +0700

    Feat: Add repository for parsing stuff from config 2

--- include/core/IGameRepository.hpp
+++ include/core/IGameRepository.hpp
@@ -13,10 +13,8 @@ class IGameRepository {
 public:
 	virtual ~IGameRepository() = default;
 
-	virtual bool save(const GameState& state, const Board& board, const TransactionLogger& logger,
-		const FestivalManager& festivals, const std::string& id) = 0;
-	virtual bool loadInto(GameState& state, Board& board, TransactionLogger& logger, FestivalManager& festivals,
-		const std::string& id) = 0;
+	virtual bool save(const GameState& state, const Board& board, const TransactionLogger& logger, const FestivalManager& festivals, const std::string& id) = 0;
+	virtual bool loadInto(GameState& state, Board& board, TransactionLogger& logger, FestivalManager& festivals, const std::string& id) = 0;
 	virtual std::vector<std::string> getPlayerNames(const std::string& id) = 0;
 	virtual bool exists(const std::string& id) const = 0;
 };
--- include/core/TextFileRepository.hpp
+++ include/core/TextFileRepository.hpp
@@ -5,10 +5,8 @@
 
 class TextFileRepository : public IGameRepository {
 public:
-	bool save(const GameState& state, const Board& board, const TransactionLogger& logger,
-		const FestivalManager& festivals, const std::string& id) override;
-	bool loadInto(GameState& state, Board& board, TransactionLogger& logger, FestivalManager& festivals,
-		const std::string& id) override;
+	bool save(const GameState& state, const Board& board, const TransactionLogger& logger, const FestivalManager& festivals, const std::string& id) override;
+	bool loadInto(GameState& state, Board& board, TransactionLogger& logger, FestivalManager& festivals, const std::string& id) override;
 	std::vector<std::string> getPlayerNames(const std::string& id) override;
 	bool exists(const std::string& id) const override;
 };
